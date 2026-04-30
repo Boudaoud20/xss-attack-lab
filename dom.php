55bc00627d29342d0639f0cdcb8327efd27fc320
Boudaoud <boudaoudabderrahmane2005@gmail.com>
2026-04-30 11:14:54 +0100

    Allow DOM-XSS attack
    Allow DOM-XSS attack by adding decodeURIComponent() method

--- dom.php
+++ dom.php
@@ -1,30 +1,35 @@
 <!DOCTYPE html>
 <html>
-<link rel="stylesheet" href="style.css">
+
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+
 <body>
 
 
-<div class="navbar">
-    <h2>XSS Lab Pro</h2>
-</div>
+    <div class="navbar">
+        <h2>XSS Lab Pro</h2>
+    </div>
 
-<div class="container">
+    <div class="container">
 
-<div class="card">
-<h2>⚡ DOM XSS</h2>
+        <div class="card">
+            <h2>⚡ DOM XSS</h2>
 
-<div id="output"></div>
+            <div id="output"></div>
 
-</div>
+        </div>
 
-</div>
+    </div>
 
-<script>
-let input = location.hash.substring(1);
+    <script>
+        let input = location.hash.substring(1);
 
-// vulnerable
-document.getElementById("output").innerHTML = input;
-</script>
+        // vulnerable
+        document.getElementById("output").innerHTML = decodeURIComponent(input);
+    </script>
 
 </body>
+
 </html>
